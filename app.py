d116d1dadbfcc4cf4b0c3cd46f6c9a8eef2ceb36
Rashmika K R <krrashmika2004@gmail.com>
2025-01-15 18:00:44 +0530

    Updated Flask app to bind to correct port

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def predict_age_group(age_group):
 
     return render_template(f'{age_group}.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
